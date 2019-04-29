05ecd09249b8129793b6aabd2c0ecbf34a82a94b
0118ayush <ayush_gehlot@hotmail.com>
2019-04-29 14:17:12 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+seedstudents = {:names => [{first_name: "Ayush", last_name: "Gehlot"}, {first_name: "iron", last_name: "man"}, {first_name: "Will", last_name: "Smith"}]}
+
+Student.create(seedstudents[:names])
